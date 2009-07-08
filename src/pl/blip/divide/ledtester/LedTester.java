dd7d97e4ba1f8dbb9ba7da9ee910666ab66253ed
Rafał Rzepecki <divided.mind@gmail.com>
2009-07-08 10:18:32 +0200

    Initial commit (project stub).

---
+++ src/pl/blip/divide/ledtester/LedTester.java
@@ -0,0 +1,13 @@
+package pl.blip.divide.ledtester;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LedTester extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
